41db9ad0efbcd96b3e545bbf8ec1bff7662f5db2
JasonP <muragami@wishray.com>
2021-10-23 12:44:10 -0400

    scripts for various platforms.

---
+++ clean.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+rm *.o
+rm *.exe
+rm *.wav
+rm ./dll/*.o
+rm ./dll/*.dylib
+rm ./dll/*.so
+rm ./dll/*.dll
---
+++ mac_test.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+g++ -m64 -s -std=c++17 -O2 -c cppSfxr.cpp -o cppSfxr.o
+g++ -m64 -s -std=c++17 -O2 -c main.cpp -o main.o
+g++ -m64 -s -std=c++17 -O2 -o main cppSfxr.o main.o
