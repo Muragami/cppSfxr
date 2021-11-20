6774ae2c1366c0d57294e08ea9a55053e3808746
JasonP <muragami@wishray.com>
2021-11-20 13:50:19 -0500

    script kiddie strikes back, episode 1

--- dll/win_dll.sh
+++ dll/win_dll.sh
@@ -2,4 +2,4 @@
 g++ -m64 -fPIC -std=c++17 -O3 -c ../cppSfxr.cpp -o ../cppSfxr.o
 g++ -m64 -fPIC -std=c++17 -O3 -c ../libSfxr.cpp -o ../libSfxr.o
 g++ -m64 -fPIC -std=c++17 -O3 -c wrap.cpp -o wrap.o
-g++ -m64 -shared -fPIC -std=c++17 -O3 -static-libstdc++ -o x64cppSfxr.dll ../cppSfxr.o ../libSfxr.o wrap.o
+g++ -m64 -shared -fPIC -std=c++17 -O3 -o x64cppSfxr.dll ../cppSfxr.o ../libSfxr.o wrap.o
